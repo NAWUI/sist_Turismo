c69a0e3268c296c54cac66ee40c8a34f420296ed
Juan-Sancz <131791245+Juan-Sancz@users.noreply.github.com>
2023-09-22 18:48:57 -0300

    cambios para muestra correcta de divs

--- js/script.js
+++ js/script.js
@@ -12,6 +12,7 @@ function openNav() {
     sidebar.classList.add("sizeChange", "sizeChangeMobile");
     oscuro.classList.add("opacidad");
     sidebarCont.style.display = "block";
+    console.log("hola")
   }
   
 function closeNav() {
@@ -23,7 +24,8 @@ function closeNav() {
 
   document.addEventListener("DOMContentLoaded", () => {
     const stands = document.querySelectorAll(".standVer, .standHor");
-  
+    
+    
     stands.forEach(stand => {
       stand.addEventListener("click", () => {
         // Primero, deselecciona todos los stands
@@ -35,6 +37,93 @@ function closeNav() {
   
         // Luego, agrega la clase "seleccionado" al stand clickeado
         stand.classList.add("seleccionado");
-      });
+
+        /*$(document).ready(function(){
+          $('#descripcion').load('php/localidades_mapa.php'); 
+          
+          
+      });*/
+
+      
+
+          var standText1=stand.textContent.replace(/[\n\r]+|[\s]{2,}/g, ' ').trim();
+          if(standText1!=""){
+            const desc1 = document.getElementById("admin");
+            const computedStyle = window.getComputedStyle(desc1);
+          
+            if (computedStyle.display == "none") {
+              desc1.style.display = "block";
+            } else {
+              desc1.style.display = "none";
+            }
+          }else{
+          const desc = document.getElementById("descripcion");
+          const computedStyle = window.getComputedStyle(desc);
+          
+            if (computedStyle.display == "none") {
+              desc.style.display = "block";
+            } else {
+              desc.style.display = "none";
+            }
+          }
+
+
+    var enviar=document.getElementById("enviar");
+      console.log(enviar);
+        enviar.addEventListener("click", () => {
+          
+          
+
+            var texto=document.getElementById("select");
+            var selectedL=texto.options[texto.selectedIndex].text;
+            var standText=stand.textContent.replace(/[\n\r]+|[\s]{2,}/g, ' ').trim();
+            var standId=stand.id;
+
+            var formData = {
+            'standId': standId,
+            'standText': standText,
+            'selectedL' : selectedL
+            }
+        $.ajax({
+            type: 'POST',
+            url: 'php/asignar_localidades.php', // Replace with your PHP file's URL
+            data: formData,
+            success: function(response) {
+                console.log('AJAX request successful:', response);
+                // You can do something with the response here
+            },
+            error: function(xhr, status, error) {
+                console.log('AJAX Error:', error);
+            }
+            });
+          })
+
+    
+
+      
+      })
     });
+    })
+    
+        
+    
+
+  
+
+ /* var enviar=document.getElementById("stand1");
+  enviar.addEventListener("click",function cargarContenido(){
+
+  /*$("stand1").click(function(){
+    cargarContenido("cargar_localidades.php");
   });
+
+  //function cargarContenido() {
+    alert("hola");
+    document.getElementById("descripcion").load("carga_contenido.php");
+    $("#Descripcion").load("../carga_localidades.php");
+ // }
+});
+*/
+
+
+  
