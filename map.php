7652843b255d644b0e42fbd74bfebeb8482c7bac
Cristofacha <santiagoramallo2005>
2023-09-22 18:24:33 -0300

    Sidebar adjust

--- map.php
+++ map.php
@@ -15,6 +15,8 @@
     <title>Sistema de Jornadas Turísticas</title>
     <link rel="stylesheet" href="css/style.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+    <!-- Option 1: Include in HTML -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script> 
     
@@ -24,55 +26,6 @@
            <?php 
                 include("header.php");
             ?>
-            <!-- HEADER QUE SE MUESTRA DE BASE 
-            <nav class="navbar navbar-light bg-customBlue" style="z-index: 1;">
-                <div class="container-fluid" id="menuToggle">
-                    <button
-                        class="navbar-toggler"
-                        type="button"
-                        data-mdb-toggle="collapse"
-                        data-mdb-target="#navbarToggleExternalContent2"
-                        aria-controls="navbarToggleExternalContent1"
-                        aria-expanded="false"
-                        aria-label="Toggle navigation"
-                        id="open"
-                    >
-                        <i class="fas fa-bars text-light"></i>
-                
-                </button>
-                 BOTON DE CERRAR SESION
-                    <a href="index.html"><button class="button-28" role="button">Cerrar Sesión</button></a>    
-                BOTON DE CERRAR SESION FIN
-                </div>
-            </nav>
-            HEADER QUE SE MUESTRA DE BASE FIN -->
-    
-            <!-- DIV DE OSCURIDAD -->
-            <div id="oscuro" class="test" style="display: none;"></div>
-            <!-- DIV DE OSCURIDAD  FIN-->
-    
-            <!-- SIDEBAR QUE APARECE -->
-            <div id="mySidenav" class="sidenav" style="z-index: 3;">
-                <div class="sidenav-content" id="sidenavContent">
-                    <a class="closebtn" id="close">&times;</a>
-                    <h1 class="sidenav-title">Sistema de Jornadas Turísticas</h1>
-                    <br>                    
-                    <div class="sideButtonsContainer">
-                        <a class="sideButtons " href="map.php">Mapa</a>
-
-                        <a class="sideButtons " href="registro.php">Registro</a>
-
-                        <a class="sideButtons " href="localidades.php">Lista de localidades</a>
-
-                        <a class="sideButtons " href="carga_grupo.php">Inscripción de Proyectos</a>
-
-                        <a class="sideButtons " href="carga_almyprof.php">Carga de Alumnos y Profesores</a>
-                    </div>
-                </div>
-            </div>
-            <!-- SIDEBAR QUE APARECE FIN -->
-    
-        </div>
         <!-- HEADER FIN -->
 
 
