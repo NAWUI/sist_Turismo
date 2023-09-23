da8f5c1e847cb2b28005513faf3d9cc4fcaa1124
Juan-Sancz <131791245+Juan-Sancz@users.noreply.github.com>
2023-09-23 04:05:36 -0300

    mapa localidades funcional

--- localidades.php
+++ localidades.php
@@ -1,3 +1,13 @@
+<?php
+
+    include("php/connection.php");
+
+    $sqlLoc="SELECT * FROM localidades WHERE 1";
+    
+    $queryLoc=mysqli_query($con,$sqlLoc);
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -10,11 +20,9 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 </head>
 <body>
-                      <!-- HEADER INICIO -->
-                      <?php 
-                include("header.php");
-            ?>
-            <!-- HEADER QUE SE MUESTRA DE BASE 
+           <!-- HEADER INICIO -->
+           <div class="headerA">
+            <!-- HEADER QUE SE MUESTRA DE BASE -->
             <nav class="navbar navbar-light bg-customBlue" style="z-index: 1;">
                 <div class="container-fluid" id="menuToggle">
                     <button
@@ -30,12 +38,12 @@
                         <i class="fas fa-bars text-light"></i>
                 
                 </button>
-                 BOTON DE CERRAR SESION
+                <!-- BOTON DE CERRAR SESION -->
                     <a href="index.html"><button class="button-28" role="button">Cerrar Sesión</button></a>    
-                BOTON DE CERRAR SESION FIN
+                <!-- BOTON DE CERRAR SESION FIN-->
                 </div>
             </nav>
-            HEADER QUE SE MUESTRA DE BASE FIN -->
+            <!-- HEADER QUE SE MUESTRA DE BASE FIN -->
     
             <!-- DIV DE OSCURIDAD -->
             <div id="oscuro" class="test" style="display: none;"></div>
@@ -64,14 +72,45 @@
     
         </div>
         <!-- HEADER FIN -->
+        <br>    
+        <br>
 
         <section class="listaLayout">
-        <div class="Lista">1</div>
-        <div class="Descripcion">
+                <div class="Descripcion">
 
-            <input type="text" id="nombreLocalidad" name="nombreLocaldiad">
+                <table class="table">
+                    <thead>
+                        <tr>
+                        <th scope="col">Localidad</th>
+                        <th scope="col">Alumnos</th>
+                        <th scope="col">Profesor</th>
+                        <th scope="col">Administrador</th>
+                        </tr>
+                    </thead>
+                    <?php while($arrayLoc=mysqli_fetch_array($queryLoc)){ 
+                        
+                        $sqlAlum="SELECT * FROM alumnos WHERE id_localidades='".$arrayLoc["id"]."'";
+                        $queryAlum=mysqli_query($con,$sqlAlum);
+                        
 
-        </div>
+                        $sqlUs="SELECT * FROM usuarios WHERE id_localidades='".$arrayLoc["id"]."'";
+                        $queryUs=mysqli_query($con,$sqlUs);
+
+                        ?>
+                    <tbody>
+                        <tr>
+                        <td><?php echo $arrayLoc["nombreLocalidad"] ?></td>
+                        <td><?php while($arrayAlum=mysqli_fetch_array($queryAlum)){ echo "-".$arrayAlum["nombre"]."<br>"; } ?></td>
+                        <td><?php echo $arrayLoc["profesorACargo"] ?></td>
+                        <td><?php while($arrayUs=mysqli_fetch_array($queryUs)){ echo "-".$arrayUs["nombre"]."<br>"; } ?></td>
+                        </tr>
+                    </tbody>
+
+                <?php } ?>
+
+                </table>
+
+            </div>
         </section>
 
     
